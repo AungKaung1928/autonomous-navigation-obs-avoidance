7d38dda7e0baa635d147a4cf5b6b976e1da08040
Aung Kaung Myat <103877301+AungKaung1928@users.noreply.github.com>
2025-08-01 10:05:19 +0900

    Update setup.py

--- simple_navigation_project/setup.py
+++ simple_navigation_project/setup.py
@@ -1,29 +1,28 @@
-from setuptools import setup, find_packages
-import os
-from glob import glob
+from setuptools import setup
 
 package_name = 'simple_navigation_project'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        ('share/' + package_name + '/launch', ['launch/nav2_simple.launch.py']),
+        ('share/' + package_name + '/config', ['config/nav2_params.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='username@gmail.com',
-    description='Simple obstacle avoidance for TurtleBot3 using laser scan data',
+    maintainer='yourname',
+    maintainer_email='you@email.com',
+    description='Simple Nav2 navigation demo',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'obstacle_avoider = simple_navigation_project.obstacle_avoider:main',
+            'obstacle_avoider = simple_navigation_project.obstacle_avoider:main'
         ],
     },
 )
