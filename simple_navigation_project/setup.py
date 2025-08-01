d6f451df2f3e2d04e0eb444b713b88180c11d1f8
Aung Kaung Myat <103877301+AungKaung1928@users.noreply.github.com>
2025-08-01 17:43:10 +0900

    Update setup.py

--- simple_navigation_project/setup.py
+++ simple_navigation_project/setup.py
@@ -12,23 +12,23 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Use glob to automatically find launch files
+        # Include launch files
         (os.path.join('share', package_name, 'launch'), 
-            glob('launch/*launch.py')),
-        # Use glob to automatically find config files  
+            glob(os.path.join('launch', '*.py'))),
+        # Include config files (if any)
         (os.path.join('share', package_name, 'config'), 
-            glob('config/*.yaml')),
+            glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='yourname',
-    maintainer_email='you@email.com', 
-    description='Simple Nav2 navigation demo with TurtleBot3',
+    maintainer_email='you@email.com',
+    description='TurtleBot3 navigation using Nav2: continuous movement with obstacle avoidance',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'obstacle_avoider = simple_navigation_project.obstacle_avoider:main'
+            'obstacle_avoider = simple_navigation_project.obstacle_avoider:main',
         ],
     },
 )
